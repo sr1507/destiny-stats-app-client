4583437f7e929908a89a980c5b5a3722e314d5fc
sr1507 <sam10rodgers@gmail.com>
2020-05-22 20:28:56 +0100

    Update config settings

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export default {
   apiGateway: {
     REGION: "eu-west-2",
-    URL: "https://jqyp1wdsmi.execute-api.eu-west-2.amazonaws.com/prod/get",
+    URL: "https://jqyp1wdsmi.execute-api.eu-west-2.amazonaws.com/prod",
   },
   cognito: {
     REGION: "eu-west-2",
-    USER_POOL_ID: "eu-west-2_iDj55rwGC",
-    APP_CLIENT_ID: "3uakhcaatlqpts02klc4j60idd",
-    IDENTITY_POOL_ID: "eu-west-2:23e55a3d-e73b-4fa9-bac0-0512366844f6"
-  }
+    USER_POOL_ID: "eu-west-2_xsHdOMhBo",
+    APP_CLIENT_ID: "703tlm7arj5ek6hackm7uuefte",
+    IDENTITY_POOL_ID: "eu-west-2:25ca2e82-3574-4486-9b19-a84263cd8141",
+  },
 };
