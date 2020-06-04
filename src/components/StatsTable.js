7153f88314ca14ca1f117e071491aaf598269c91
sr1507 <sam10rodgers@gmail.com>
2020-06-04 19:06:00 +0100

    Added extra data to stats table

--- src/components/StatsTable.js
+++ src/components/StatsTable.js
@@ -7,6 +7,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { getGamemodeDetails } from "../libs/gamemodeLib.js"
+import { round } from "../libs/utilitiesLib.js"
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -38,14 +40,21 @@ export default function CustomizedTables(input) {
 
   const classes = useStyles();
   
-  function createData(gamemode, KD) {
-    return { gamemode, KD };
+  function createData(gamemode, KD, KDA, kills, assists, averageKills, winrate, wins) {
+    return { gamemode, KD, KDA, kills, assists, averageKills, winrate, wins };
   }
   
   function createRows(input){
       var rows = [];
       for(var gamemode of input.stats.totals.gamemodeList){
-          rows.push(createData(gamemode, input.stats.killsDeathsRatio[gamemode]));
+        var details = getGamemodeDetails(gamemode);
+        if(details && details.display){
+          var winrate = round(input.stats.winRate[gamemode]*100, 1) + "%";
+          var KD = round(input.stats.killsDeathsRatio[gamemode], 2);
+          var KDA = round(input.stats.killsDeathsAssists[gamemode], 2);
+          var averageKills = round(input.stats.averageKills[gamemode], 1); 
+          rows.push(createData(details.name, KD, KDA, input.stats.totals.kills[gamemode], input.stats.totals.assists[gamemode], averageKills, winrate, input.stats.totals.wins[gamemode]));
+        }
       }
       return rows;
   }
@@ -59,6 +68,12 @@ export default function CustomizedTables(input) {
           <TableRow>
             <StyledTableCell>Gamemode</StyledTableCell>
             <StyledTableCell align="right">KD</StyledTableCell>
+            <StyledTableCell align="right">KDA</StyledTableCell>
+            <StyledTableCell align="right">Kills</StyledTableCell>
+            <StyledTableCell align="right">Assists</StyledTableCell>
+            <StyledTableCell align="right">Average Kills</StyledTableCell>
+            <StyledTableCell align="right">Winrate</StyledTableCell>
+            <StyledTableCell align="right">Wins</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -68,6 +83,12 @@ export default function CustomizedTables(input) {
                 {row.gamemode}
               </StyledTableCell>
               <StyledTableCell align="right">{row.KD}</StyledTableCell>
+              <StyledTableCell align="right">{row.KDA}</StyledTableCell>
+              <StyledTableCell align="right">{row.kills}</StyledTableCell>
+              <StyledTableCell align="right">{row.assists}</StyledTableCell>
+              <StyledTableCell align="right">{row.averageKills}</StyledTableCell>
+              <StyledTableCell align="right">{row.winrate}</StyledTableCell>
+              <StyledTableCell align="right">{row.wins}</StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
