183cf694e471f96e48f9eeb04c21f3eda20c932f
sr1507 <sam10rodgers@gmail.com>
2020-06-05 18:30:29 +0100

    Removed login requirement to use api

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -33,10 +33,6 @@ export default function Home() {
   }
 
   return (<div className="Home">
-    {
-      isAuthenticated
-        ? renderStats()
-        : renderLander()
-    }
+    {renderStats()}
   </div>);
 }
