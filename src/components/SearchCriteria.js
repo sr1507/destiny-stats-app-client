1cc533604f5495550bf034f4db90d3a19be46b12
sr1507 <sam10rodgers@gmail.com>
2020-06-12 18:31:14 +0100

    Added functionality to disable button when no display name has been entered

--- src/components/SearchCriteria.js
+++ src/components/SearchCriteria.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import DateSlider from "./DateSlider";
 import DisplayNameTextField from "./DisplayNameTextField";
@@ -6,22 +6,29 @@ import Button from "@material-ui/core/Button";
 
 export default function SearchCriteria({ setDisplayName, setStartDate, onSubmit }) {
 
+  const [disabledButton, setDisabledButton] = useState(true);
+
   function handleSubmitClick(event) {
     event.preventDefault();
     onSubmit();
+  }
+
+  function handleDisplayNameChange(displayName){
+    setDisplayName(displayName);
+    setDisabledButton(displayName === "");
   }
 
   return (
     <form autoComplete="off">
     <Grid container direction="column" alignItems="center" spacing={3}>
       <Grid item>
-        <DisplayNameTextField handleDisplayNameChange={setDisplayName} />
+        <DisplayNameTextField handleDisplayNameChange={handleDisplayNameChange} />
       </Grid>
       <Grid item>
         <DateSlider handleDateChange={setStartDate} />
       </Grid>
       <Grid item>
-        <Button variant="outlined" color="primary" onClick={handleSubmitClick} type="submit">
+        <Button variant="outlined" color="primary" onClick={handleSubmitClick} type="submit" disabled={disabledButton}>
           Search
         </Button>
       </Grid>
