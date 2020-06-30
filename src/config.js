351b9d85116b0692e658c06dbe73658ecbd1ce68
sr1507 <sam10rodgers@gmail.com>
2020-06-30 18:49:01 +0100

    Update api url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   apiGateway: {
     REGION: "eu-west-2",
-    URL: "https://jqyp1wdsmi.execute-api.eu-west-2.amazonaws.com/prod"
+    URL: "https://zeqjtmyy0k.execute-api.eu-west-2.amazonaws.com/prod"
   },
   cognito: {
     REGION: "eu-west-2",
